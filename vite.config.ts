353ea5744f70b22b16aa28208d1d4ddc16dc9624
Rqtzbot <146736611+Rqtzbot@users.noreply.github.com>
2026-04-27 14:59:01 +0800

    改造pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,35 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['robot-solid-full.svg'],
+        manifest: {
+          name: 'looraysBot',
+          short_name: 'looraysBot',
+          description: 'looraysBot Application',
+          theme_color: '#ffffff',
+          start_url: '.',
+          display: 'standalone',
+          background_color: '#ffffff',
+          icons: [
+            {
+              src: 'robot-solid-full.svg',
+              sizes: 'any',
+              type: 'image/svg+xml'
+            }
+          ]
+        }
+      })
+    ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
